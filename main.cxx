b53831ef3b340c2e44ff4294991734bbd7f40d95
TomNin97 <chm16@zips.uakron.edu>
2020-10-21 17:15:48 -0400

    Added Feat struct

--- main.cxx
+++ main.cxx
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 int main()
 {
-  std::cout << "This the main code for the program" << std::endl;
-  return 0;
+	/* The standard structure for Feats the vector would hold */
+	struct Feat
+	{
+		std::string name;  // Feat Name
+		std::string type;  // Feat types
+		std::vector<std::string> genPrereq;  // Prerequisites for Feat to be mentioned in UML
+		std::vector<std::string> featPrereq;  // List of Prerequisites which are also other Feats
+		std::string benefits;  // Description of benefits
+		std::vector<std::string> special;  // Special notes on feats
+		std::vector<std::string> sources;  // Source Books
+	}
+
+	std::cout << "This the main code for the program" << std::endl;
+	return 0;
 }
