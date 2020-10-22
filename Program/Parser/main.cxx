3fdbf73284423490e620b6e78a4fd85732a36542
TomNin97 <chm16@zips.uakron.edu>
2020-10-21 22:03:51 -0400

    Added UI to main

--- Program/Parser/main.cxx
+++ Program/Parser/main.cxx
@@ -1,13 +1,41 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <limits>
+#include <ios>
 
 #include "Feats.h"
 
 int main()
 {
 	std::vector<Feat> featList;
+	char input;
 
-	std::cout << "This the main code for the program" << std::endl;
+	std::cout << "FEAT PROGRESSION CHART" << std::endl;
+	while(true)
+	{
+		std::cout << "Enter input (i), Process output (o), or Exit (x):" << std::endl;
+		std::cout << "-> ";
+		std::cin >> input;
+
+		if (input == 'i')
+		{
+			std::cout << "\nYou've selected input!" << std::endl;
+		}
+
+		else if (input == 'o')
+		{
+			std::cout << "\nYou've selected output!" << std::endl;
+		}
+		
+		else if (input == 'x')
+		{
+			break;
+		}
+
+		std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	}
+
 	return 0;
 }
