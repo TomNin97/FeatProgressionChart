7f0b7fcf5ed0c4d7727632c90e2d740406743a2c
TomNin97 <chm16@zips.uakron.edu>
2020-10-24 19:28:52 -0400

    Added compiler for UML diagram

--- Parser/UML/uml.cxx
+++ Parser/UML/uml.cxx
@@ -23,6 +23,36 @@ void umlOut(std::vector<Feat> featList)
 	for(i=0; i < featList.size(); i++) // iterates through feats
 	{
 		scribe << "\nclass \"" << featList[i].name << "\" {\n"; // gives feat name
+		scribe << "  .. " << featList[i].type << " ..\n"; // gives feat type
+
+		scribe << "\n  .. " << "Prerequisites" << " ..\n"; // headlines feat prerequisites
+		for(j=0; j<featList[i].genPrereq.size(); j++)
+		{
+			scribe << "-" << featList[i].genPrereq[j] << "\n";
+		}
+
+		scribe << "\n  .. " << "Benefits" << " ..\n"; // headlines feat Benefits
+		scribe << "+" << featList[i].benefits << "\n";
+
+		scribe << "\n  .. " << "Special" << " ..\n"; // headlines feat Specials
+		for(j=0; j<featList[i].special.size(); j++)
+		{
+			scribe << "#" << featList[i].special[j] << "\n";
+		}
+
+		scribe << "\n  .. " << "Source Books" << " ..\n"; // headlines source books
+		for(j=0; j<featList[i].sources.size(); j++)
+		{
+			scribe << "~" << featList[i].sources[j] << "\n";
+		}
+
+		scribe << "}\n"; // closes out class
+
+		/* Class Relations */
+		for(j=0; j<featList[i].featPrereq.size(); j++)
+		{
+			scribe << "\"" << featList[i].name << "\" --> \"" << featList[i].featPrereq[j] << "\"\n";
+		}
 	}
 
 	scribe << "\n@enduml"; // End of file
